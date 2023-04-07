bb73cfaf75c3ffad70ff6e137abee2e8d8756a85
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-04-07 18:44:15 -0300

    Header file `*/io.h` is not need and may cause `include errors`

--- Source/FileList.cpp
+++ Source/FileList.cpp
@@ -14,22 +14,10 @@
 
 #include <stdio.h> // RAKNET_DEBUG_PRINTF
 #include "RakAssert.h"
-#if defined(ANDROID)
-#include <asm/io.h>
-#elif defined(_WIN32) || defined(__CYGWIN__)
-#include <io.h>
-
-
-#elif !defined ( __APPLE__ ) && !defined ( __APPLE_CC__ ) && !defined ( __PPC__ ) && !defined ( __FreeBSD__ ) && !defined ( __S3E__ )
-#include <sys/io.h>
-#endif
-
 
 #ifdef _WIN32 
 // For mkdir
 #include <direct.h>
-
-
 #else
 #include <sys/stat.h>
 #endif
